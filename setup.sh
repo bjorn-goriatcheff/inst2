fb7bdb65b1c16e1cb0a5e2cb8422a3e5518ca315
root <root@instance-4.europe-west2-c.c.hsbc-env.internal>
2019-02-27 15:44:05 +0000

    testing script release

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
+#Epel tools RHEL 7
+rpm -Uvh epel-release-latest-7.noarch.rpm
+
+#Decompress archives
 tar xf dep2.tar.xz
 tar xf dep3.tar.xz
 tar xf pip.tar.xz
-tar xf lib.tar.xz
+tar xf np.tar.xz
+tar xf six.tar.xz
+tar xf pd.tar.xz
+tar xf pw.tar.xz
 
 #GCC Make etc..
 rpm -Uvh ./dep2/*.rpm
@@ -17,10 +24,11 @@ source ./vec_ai_env/bin/activate
 #numpy install
 rpm -Uvh ./numpy-rpm/*.rpm
 rpm -Uvh ./six-rpm/*.rpm
+rpm -Uvh ./pandas-rpm/*.rpm
+rpm -Uvh ./pillow-rpm/*.rpm
 
 #Pip modules install
 python3 -m pip install --no-index --find-links=./pip-modules/ ./pip-modules/statistics-1.0.3.5.tar.gz
 python3 -m pip install --no-index --find-links=./pip-modules/ ./pip-modules/StringDist-1.0.9.tar.gz
-python3 -m pip install --no-index --find-links=./pip-modules/ ./pip-modules/Pillow-5.4.1.tar.gz
 
 
